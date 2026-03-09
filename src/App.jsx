2c1a4af528831e01858ea36e181422705ff3d443
Jesimiel <name@MacBook-Pro-de-Jesimiel-Pereira.local>
2026-03-09 18:19:23 -0300

    Removendo LP da Vercel para isolar em outro dominio

--- src/App.jsx
+++ src/App.jsx
@@ -19,35 +19,34 @@ import Financeiro from './pages/Financeiro';
 import Tarefas from './pages/Tarefas';
 import WhiteLabel from './pages/WhiteLabel';
 import Prospecting from './pages/Prospecting';
-import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
 
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/clientes" element={<Clients />} />
-            <Route path="/clientes/:id" element={<ClientDetail />} />
-            <Route path="/leads" element={<Leads />} />
-            <Route path="/prospeccao" element={<Prospecting />} />
-            <Route path="/tarefas" element={<Tarefas />} />
-            <Route path="/reunioes" element={<Meetings />} />
-            <Route path="/financeiro" element={<Financeiro />} />
-            <Route path="/trafego" element={<Traffic />} />
-            <Route path="/academy" element={<Academy />} />
-            <Route path="/metas" element={<Goals />} />
-            <Route path="/pesquisas" element={<Surveys />} />
-            <Route path="/docs" element={<Settings />} />
-            <Route path="/configuracoes" element={<Settings />} />
-            <Route path="/inbox" element={<Inbox />} />
-            <Route path="/fluxos" element={<FlowEditor />} />
-            <Route path="/templates" element={<Templates />} />
-            <Route path="/whitelabel" element={<WhiteLabel />} />
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="clientes" element={<Clients />} />
+            <Route path="clientes/:id" element={<ClientDetail />} />
+            <Route path="leads" element={<Leads />} />
+            <Route path="prospeccao" element={<Prospecting />} />
+            <Route path="tarefas" element={<Tarefas />} />
+            <Route path="reunioes" element={<Meetings />} />
+            <Route path="financeiro" element={<Financeiro />} />
+            <Route path="trafego" element={<Traffic />} />
+            <Route path="academy" element={<Academy />} />
+            <Route path="metas" element={<Goals />} />
+            <Route path="pesquisas" element={<Surveys />} />
+            <Route path="docs" element={<Settings />} /> {/* Simulated page for docs */}
+            <Route path="configuracoes" element={<Settings />} />
+            <Route path="inbox" element={<Inbox />} />
+            <Route path="fluxos" element={<FlowEditor />} />
+            <Route path="templates" element={<Templates />} />
+            <Route path="whitelabel" element={<WhiteLabel />} />
           </Route>
         </Routes>
       </Router>
